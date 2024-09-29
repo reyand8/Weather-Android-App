f038feb137488cca1d8fb18fda43c943c3c09ed6
Andrey <andreeey.gg@gmail.com>
2024-09-29 16:24:29 +0200

    add DialogManager

---
+++ app/src/main/java/com/example/weather_android_app/DialogManager.kt
@@ -0,0 +1,42 @@
+package com.example.weather_android_app
+
+import android.app.AlertDialog
+import android.content.Context
+import android.widget.EditText
+
+object DialogManager {
+    fun locationSettingsDialog(context: Context, listener: Listener){
+        val builder = AlertDialog.Builder(context)
+        val dialog = builder.create()
+        dialog.setTitle("GPS disabled!")
+        dialog.setMessage("Do you want to enable GPS?")
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK"){_,_, ->
+            listener.onClick(null)
+            dialog.dismiss()
+        }
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel"){_,_, ->
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    fun searchByNameDialog(context: Context, listener: Listener){
+        val builder = AlertDialog.Builder(context)
+        val edName = EditText(context)
+        builder.setView(edName)
+        val dialog = builder.create()
+        dialog.setTitle("Your city:")
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK"){_,_, ->
+            listener.onClick(edName.text.toString())
+            dialog.dismiss()
+        }
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel"){_,_, ->
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    interface Listener{
+        fun onClick(name: String?)
+    }
+}
