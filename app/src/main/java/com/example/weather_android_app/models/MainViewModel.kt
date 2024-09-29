f8556c1d9cb76cb62204b0a1bec4d1612b90f806
Andrey <andreeey.gg@gmail.com>
2024-09-29 16:15:02 +0200

    add models

---
+++ app/src/main/java/com/example/weather_android_app/models/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.weather_android_app.models
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
+}
